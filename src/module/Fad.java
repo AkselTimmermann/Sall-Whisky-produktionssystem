2bb70654911057288858f02603aca1ce0ae56492
AkselTimmermann <aksel.timmermann@gmail.com>
2026-05-01 09:34:21 +0200

    Fad og lager klasse skeletet færdigt.

--- src/module/Fad.java
+++ src/module/Fad.java
@@ -1,4 +1,38 @@
 package module;
 
 public class Fad {
+    private String fadId;
+    private String traaType;
+    private String beskrivelse;
+    private double stoerrelse;
+    private String plads;
+    private Lager lager;
+
+    public Fad(String fadId, String traaType, String beskrivelse, double stoerrelse, String plads) {
+        this.fadId = fadId;
+        this.traaType = traaType;
+        this.beskrivelse = beskrivelse;
+        this.stoerrelse = stoerrelse;
+        this.plads = plads;
+    }
+
+    public String getFadId() {
+        return fadId;
+    }
+
+    public String getTraaType() {
+        return traaType;
+    }
+
+    public String getBeskrivelse() {
+        return beskrivelse;
+    }
+
+    public double getStoerrelse() {
+        return stoerrelse;
+    }
+
+    public String getPlads() {
+        return plads;
+    }
 }
--- src/module/Lager.java
+++ src/module/Lager.java
@@ -1,4 +1,11 @@
 package module;
 
+
+import java.util.ArrayList;
+
 public class Lager {
+    private String navn;
+    private String lokation;
+    private int kapacitet;
+    private ArrayList<Fad> fade = new ArrayList<>();
 }
