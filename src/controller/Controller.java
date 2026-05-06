b7ff45089e256a2680f5f7beaa65fc42556501fe
CappiDell <caper.christensen@gmail.com>
2026-05-06 10:10:59 +0200

    Storage interface

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,4 +1,14 @@
 package controller;
 
+import storage.Storage;
+import storage.StorageInterface;
+
 public class Controller {
+    private StorageInterface storage;
+
+    Controller(StorageInterface storage){
+    this.storage = storage;
+    }
+
+
 }
