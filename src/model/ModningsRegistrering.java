e963e41268e690d9d0800f8fb610b0486ed88f91
CappiDell <caper.christensen@gmail.com>
2026-05-06 11:49:50 +0200

    ModningsRegistrering

--- src/model/ModningsRegistrering.java
+++ src/model/ModningsRegistrering.java
@@ -1,4 +1,25 @@
 package model;
 
+import java.time.LocalDate;
+
 public class ModningsRegistrering {
+    private double alkoholProcent;
+    private LocalDate dato;
+    private double antalLiter;
+    private String note;
+    private String titel;
+
+    public ModningsRegistrering(double alkoholProcent, LocalDate dato, double antalLiter, String note, String titel) {
+        if (alkoholProcent<0){
+            throw new IllegalArgumentException("Du kan ikke have negativ alkoholprocent");
+        }
+        if (antalLiter<0){
+            throw new IllegalArgumentException("Der skal være mere mindst 0 liter tilbage");
+        }
+        this.alkoholProcent = alkoholProcent;
+        this.dato = dato;
+        this.antalLiter = antalLiter;
+        this.note = note;
+        this.titel = titel;
+    }
 }
