657157059831fd8c8fc628d0617fcf5d39b693ac
rasjo <rasmus.joergensen@outlook.com>
2026-05-06 08:53:01 +0200

    Tilføjet felter og tomme metoder i Storage

--- src/storage/Storage.java
+++ src/storage/Storage.java
@@ -1,4 +1,38 @@
 package storage;
 
+import model.Destillering;
+import model.Fad;
+import model.Lager;
+
+import java.util.ArrayList;
+
 public class Storage {
+    private static ArrayList<Destillering> destilleringer;
+    private static ArrayList<Fad> fade;
+    private static ArrayList<Lager> lagre;
+
+
+    public static void addDestillering(Destillering destillering) {
+
+    }
+
+    public static void addFad(Fad fad) {
+
+    }
+
+    public static void addLager(Lager lager) {
+
+    }
+
+    public static ArrayList<Destillering> getDestilleringer() {
+        return null;
+    }
+
+    public static ArrayList<Fad> getFade() {
+        return null;
+    }
+
+    public static ArrayList<Lager> getLagre() {
+        return null;
+    }
 }
