f4c19a61ba4ae39f60d3e65d4eb2aa4a65b741e1
AkselTimmermann <aksel.timmermann@gmail.com>
2026-05-06 11:49:10 +0200

    PaafyldningsRegistrering oprettet

--- src/model/PaafyldningsRegistrering.java
+++ src/model/PaafyldningsRegistrering.java
@@ -1,4 +1,42 @@
 package model;
 
+import java.time.LocalDate;
+
 public class PaafyldningsRegistrering {
+    private double antalLiter;
+    private LocalDate dato;
+    private Destillering destillering;
+    private Fad fad;
+
+    public PaafyldningsRegistrering(double antalLiter, LocalDate dato, Destillering destillering, Fad fad) {
+        if (destillering == null) {
+            throw new IllegalStateException("Destillering skal være oprettet");
+        }
+        if (antalLiter > destillering.getMaengdeVaeske()) {
+            throw new IllegalStateException("Antal liter overstiger mængden af ledig destillering");
+        }
+        if (fad == null) {
+            throw new IllegalStateException("Der skal være et ledigt fad");
+        }
+        this.antalLiter = antalLiter;
+        this.dato = dato;
+        this.destillering = destillering;
+        this.fad = fad;
+    }
+
+    public double getAntalLiter() {
+        return antalLiter;
+    }
+
+    public LocalDate getDato() {
+        return dato;
+    }
+
+    public Destillering getDestillering() {
+        return destillering;
+    }
+
+    public Fad getFad() {
+        return fad;
+    }
 }
