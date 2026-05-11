e9968134c1c2c4216e270042f4a6e1fb38ab57d4
AkselTimmermann <aksel.timmermann@gmail.com>
2026-05-11 11:56:19 +0200

    OpretFadPane færdig

--- src/gui/OpretFadPane.java
+++ src/gui/OpretFadPane.java
@@ -7,6 +7,10 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import model.Fad;
+import model.Leverandoer;
+
+import java.util.Locale;
 
 public class OpretFadPane extends BorderPane {
 
@@ -17,11 +21,11 @@ public class OpretFadPane extends BorderPane {
         initContent();
     }
 
-    Label fadId = new Label("FadId:");
-    Label traaType = new Label("Træ Type:");
-    Label stoerrelse = new Label("Størrelse:");
-    Label beskrivelse = new Label("Beskrivelse:");
-    Label leverandoer = new Label("Leverandør:");
+    Label fadIdLbl = new Label("FadId:");
+    Label traaTypeLbl = new Label("Træ Type:");
+    Label stoerrelseLbl = new Label("Størrelse:");
+    Label beskrivelseLbl = new Label("Beskrivelse:");
+    Label leverandoerLbl = new Label("Leverandør:");
     TextField fadIdTxf = new TextField();
     TextField traaTypeTxf = new TextField();
     TextField stoerrelseTxf = new TextField();
@@ -58,19 +62,19 @@ public class OpretFadPane extends BorderPane {
         stoerrelseTxf.setPrefWidth(350);
         traaTypeTxf.setPrefWidth(350);
 
-        pane.add(fadId, 0, 1);
+        pane.add(fadIdLbl, 0, 1);
         pane.add(fadIdTxf, 1, 1);
 
-        pane.add(traaType, 0, 2);
+        pane.add(traaTypeLbl, 0, 2);
         pane.add(traaTypeTxf, 1, 2);
 
-        pane.add(stoerrelse, 0, 3);
+        pane.add(stoerrelseLbl, 0, 3);
         pane.add(stoerrelseTxf, 1, 3);
 
-        pane.add(leverandoer, 0,4);
+        pane.add(leverandoerLbl, 0,4);
         pane.add(leverandoerCb,1,4);
 
-        pane.add(beskrivelse, 0, 5);
+        pane.add(beskrivelseLbl, 0, 5);
         pane.add(beskriveseTa, 1, 5);
 
         HBox buttons = new HBox(btnOpret, btnRyd);
@@ -99,16 +103,53 @@ public class OpretFadPane extends BorderPane {
 
     private void opretFadAction() {
         try {
+            String fadId = fadIdTxf.getText().trim();
+            String traaType = traaTypeTxf.getText().trim();
+            String stoerrelseTxt = stoerrelseTxf.getText().trim();
+            Leverandoer leverandoer = (Leverandoer) leverandoerCb.getSelectionModel().getSelectedItem();
+            String beskrivelse = beskriveseTa.getText();
 
-        } catch (NumberFormatException e) {
-            visFejl(e.getMessage());
+            if (fadId.isEmpty()) {
+                visFejl("Fad id skal udfyldes");
+                return;
+            }
+            if (traaType.isEmpty()) {
+                visFejl("Træ type skal udfyldes");
+                return;
+            }
+            if (stoerrelseTxt.isEmpty()) {
+                visFejl("Størrelse skal udfyldes");
+                return;
+            }
+            if(leverandoer == null) {
+                visFejl("Leverandør skal vælges");
+            }
+            if (beskrivelse.isEmpty()) {
+                visFejl("Beskrivelse skal udfyldes");
+                return;
+            }
+            int stoerrelse = Integer.parseInt(stoerrelseTxt);
+            if (stoerrelse <= 0) {
+                visFejl("Størrelse skal være større end 0");
+                return;
+            }
+
+            controller.createFad(fadId, traaType, beskrivelse, stoerrelse, leverandoer);
+            visInfo("Fad blev oprettet med fadId: " + fadId + ", træ type: " + traaType + ", størrelse:" + stoerrelse + ", Leverandør:" + leverandoer);
+
+            rydFelterAction();
+
         } catch (Exception e) {
             visFejl(e.getMessage());
         }
     }
 
     private void rydFelterAction() {
-
+        fadIdTxf.clear();
+        traaTypeTxf.clear();
+        stoerrelseTxf.clear();
+        leverandoerCb.getSelectionModel().clearSelection();
+        beskriveseTa.clear();
     }
 
     private void visFejl(String besked) {
