03878018d01c3e0056b51eb06c501a341eca8e90
rasjo <rasmus.joergensen@outlook.com>
2026-05-13 12:59:54 +0200

    Oprettet sidste 2 nye panes: OpretWhiskyProduktPane og RegistrerFlaskningPane.

---
+++ src/gui/RegistrerFlaskningPane.java
@@ -0,0 +1,50 @@
+package gui;
+
+import controller.Controller;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
+public class RegistrerFlaskningPane extends BorderPane {
+
+    private final Controller controller;
+
+    public RegistrerFlaskningPane(Controller controller) {
+        this.controller = controller;
+        initContent();
+    }
+
+    private void initContent() {
+        VBox root = new VBox(10);
+        root.setPadding(new Insets(30, 60, 30, 60));
+
+        VBox header = createHeader();
+        GridPane format = createFormat();
+
+        root.getChildren().addAll(header, format);
+        this.setCenter(root);
+    }
+
+    private VBox createHeader() {
+        Label title = new Label("Registrer flaskning");
+        title.setStyle("-fx-font-size: 26px; -fx-font-weight: bold;");
+
+        Label subtitle = new Label("Registrer påfyldning af et whiskyprodukt på flasker.");
+        subtitle.setStyle("-fx-font-size: 16px;");
+
+        return new VBox(5, title, subtitle);
+    }
+
+    private GridPane createFormat() {
+        GridPane pane = new GridPane();
+        pane.setPadding(new Insets(30));
+        pane.setVgap(15);
+        pane.setHgap(15);
+        pane.setStyle("-fx-border-color: black; -fx-border-width: 1;");
+
+
+        return pane;
+    }
+}
