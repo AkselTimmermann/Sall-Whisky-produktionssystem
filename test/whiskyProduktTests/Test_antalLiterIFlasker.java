046ef72f3219d7d373b69df11574d69cd8f1fc75
CappiDell <caper.christensen@gmail.com>
2026-05-17 13:28:24 +0200

    a

--- test/whiskyProduktTests/Test_antalLiterIFlasker.java
+++ test/whiskyProduktTests/Test_antalLiterIFlasker.java
@@ -2,12 +2,26 @@ package whiskyProduktTests;
 
 import model.Flaske;
 import model.FlaskeSamling;
+import model.WhiskyProdukt;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Test_antalLiterIFlasker {
-    @Mock
-    FlaskeSamling flaskeSamling1 = new FlaskeSamling(1);
-    @Mock
-    FlaskeSamling flaskeSamling2 = new FlaskeSamling(2);
+    private @Mock FlaskeSamling flaskeSamling;
+
+    private WhiskyProdukt whiskyProdukt;
+
+
+
+    @BeforeEach
+    void setup(){
+
+    }
+
+    @Test
 
 }
