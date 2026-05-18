e523ccf2d744f7fbbb3fd98ab469182bafd337e9
CappiDell <caper.christensen@gmail.com>
2026-05-18 14:42:19 +0200

    a

--- src/model/WhiskyProdukt.java
+++ src/model/WhiskyProdukt.java
@@ -56,7 +56,14 @@ public class WhiskyProdukt {
             throw new IllegalArgumentException("Der skal oprettes mindst 1 flaske");
         }
         ArrayList<Flaske> oprettedeFlasker = new ArrayList<>();
-        int startFlaskenr = this.flasker.getLast().getFlaskeNr() +1;
+        int startFlaskenr;
+        if (this.flasker.isEmpty()){
+            startFlaskenr = 1;
+        }
+        else {
+            startFlaskenr = this.flasker.getLast().getFlaskeNr() +1;
+        }
+
         for (int i = startFlaskenr; i < startFlaskenr + antal ; i++) {
             Flaske flaske = new Flaske(i,stoerrelse,flaskeSamling, this);
             oprettedeFlasker.add(flaske);
@@ -136,6 +143,10 @@ public class WhiskyProdukt {
 
     public ArrayList<ProduktRegistrering> getProduktRegistreringer() {
         return new ArrayList<>(produktRegistreringer);
+    }
+
+    public ArrayList<Flaske> getFlasker() {
+        return new ArrayList<>(flasker);
     }
 
     public String toString() {
